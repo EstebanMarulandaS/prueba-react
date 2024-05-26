c8a50b4d6ebaa80c24dc9140a37965302ea4e18e
Esteban Marulanda <esteb_95@hotmail.com>
2024-05-26 12:35:20 -0500

    Finish home style

--- src/components/Home.jsx/Home.jsx
+++ src/components/Home.jsx/Home.jsx
@@ -1,13 +1,19 @@
+import { Link } from "react-router-dom";
 import "./home.scss";
-export const Home = () => {
-  return (
-    <div>
-      <h1>Bienvenido a la prueba tecnica de React</h1>
 
+export function Home() {
+  return (
+    <div className="home">
       <div className="btnDiv">
-        <button className="homeBtn">Mostrar lista de tareas</button>
-        <button className="homeBtn">Mostrar llamado a la APIs</button>
+        <h1 className="homeh1">Bienvenido a la prueba de React</h1>
+
+        <button className="homeBtn">
+          <Link to="/listaDeTareas">Mostrar lista de tareas</Link>
+        </button>
+        <button className="homeBtn">
+          <Link to="/api">Mostrar llamado a la APIs</Link>
+        </button>
       </div>
     </div>
   );
-};
+}
