2049979dea6267d1c81ab8d43b9889fbe66a0c86
Esteban Marulanda <esteb_95@hotmail.com>
2024-05-26 13:32:02 -0500

    add api call and style

--- src/components/Api/ApiCall.jsx
+++ src/components/Api/ApiCall.jsx
@@ -1,7 +1,57 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import "./apicall.scss";
+
 export const ApiCall = () => {
+  const [breeds, setBreeds] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (isLoading) {
+      axios
+        .get("https://dogapi.dog/api/v2/breeds")
+        .then((response) => {
+          setBreeds(response.data.data);
+          setIsLoading(false);
+        })
+        .catch((error) => {
+          console.error("Error retrieving data:", error);
+          setIsLoading(false);
+        });
+    }
+  }, [isLoading]);
+
+  const handleMakeApiCall = () => {
+    setIsLoading(true);
+  };
+
   return (
-    <div>
-      <h1>Soy la Api Call</h1>
+    <div className="apiDiv">
+      <Button
+        onClick={handleMakeApiCall}
+        variant="primary"
+        className="m-3 apiBtn"
+      >
+        Make API Call
+      </Button>
+      <div className="row">
+        {breeds.map((breed) => (
+          <div className="col-lg-4 col-md-6 mb-4" key={breed.id}>
+            <Card className="card">
+              <Card.Body>
+                <Card.Title style={{ color: "orange" }}>
+                  {breed.attributes.name}
+                </Card.Title>
+                <Card.Text style={{ color: "white" }}>
+                  {breed.attributes.description}
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
